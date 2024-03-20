5fd0f59538bc7f39ac5d6e0ce552f4b1167115eb
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-03-20 19:54:38 -0300

    Initial commit

---
+++ hello world.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+string nome;
+
+int idade;
+
+int main(){
+
+
+cout<<"Digite seu nome: "<<endl;
+cin>>nome;
+cout<<"Digite sua idade: "<<endl;
+cin>>idade;
+cout<<"Seu nome e "<<nome<<" e sua idade e "<<idade;
+
+return 0;
+
+}
