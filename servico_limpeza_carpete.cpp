37ca4669be6e8586d702d693a5ed6473b8a8b232
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-03-20 21:46:00 -0300

    nova atividade

---
+++ servico_limpeza_carpete.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+const double valor = 150;
+const double imposto = 0.06;
+const int tempo = 30;
+int quartos;
+int main(){
+
+    cout<<"Bem vindo ao servico de limpeza do Frank"<<endl;
+    cout<<"Quantos quartos voce gostaria de limpar? ";
+    cin>>quartos;
+
+    double valor_limpeza = (quartos*valor);
+    double valor_imposto_pago = (valor_limpeza*imposto);
+    double valor_total = (valor_limpeza+valor_imposto_pago);
+
+    cout<<"Orcamento para servico de limpeza de carpetes:"<<endl;
+    cout<<"Numero de quartos: "<<quartos;
+    cout<<"Preco por quarto: "<<valor;
+    cout<<"Custo: "<<valor_limpeza;
+    cout<<"Imposto: "<<valor_imposto_pago<<endl;
+    cout<<"==========================================="<<endl;
+    cout<<"Orcamento total: R$ "<<valor_total<<endl;
+    cout<<"Orcamento valido por "<<tempo;
+
+    return 0;
+}
