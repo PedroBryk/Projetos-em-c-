4fe7517c4b015ab889c984d7fccb7a0dccfbc44b
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-03-27 12:32:33 -0300

    conversor de medidas

---
+++ conversor_medidas.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double valor_metros;
+
+    cout<<"Dgite o valor em metros para conversao: ";
+    cin>>valor_metros;
+
+    double valor_decimetro = valor_metros*10;
+    double valor_centimetro = valor_metros/100;
+    double valor_milimetro = valor_metros/1000;
+
+    cout<<"O valor em decimetros eh: "<<valor_decimetro<<endl;
+    cout<<"O valor em centimetros eh: "<<valor_centimetro<<endl;
+    cout<<"O valor em milimetros eh: "<<valor_milimetro<<endl;
+
+}
