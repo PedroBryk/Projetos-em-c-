bebb108954032e60e4f5764cf5e30bbaf0a93661
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-03-29 10:27:01 -0300

    verificação de numero negativo

---
+++ lista xx/verificacao_numero_negativo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int valor;
+
+    cout<<"Digite um valor: ";
+    cin>>valor;
+
+    if(valor < 0){
+
+        cout<<"O numero digitado eh um valor negativo!";
+
+    }else if(valor == 0){
+
+        cout<<"O numero digitado eh igual a zero!";
+
+    }else if(valor > 0){
+
+        cout<<"O numero digitado eh um valor positivo!";
+
+    }
+}
