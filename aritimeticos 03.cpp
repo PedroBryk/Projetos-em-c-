1209d0efb4e7324f2887083cf726cf67170a2ca5
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-03-29 10:17:14 -0300

    verificação

---
+++ aritimeticos 03.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+    int valor;
+
+    cout<<"Digite um valor: ";
+    cin>>valor;
+    cout<<boolalpha;
+
+    bool fora_limites = false;
+    fora_limites = (valor >= 5)&&(valor <=50);
+    cout<<valor<<" esta entre 5 e 50? "<<fora_limites;
+
+ }
