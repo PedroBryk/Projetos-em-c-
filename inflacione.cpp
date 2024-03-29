51af6f12aacbcf60b36c79e4c3a53dd3c367d0c4
Pedro Bryk <88406806+PedroBryk@users.noreply.github.com>
2024-03-29 09:53:17 -0300

    valor inflacionado

--- inflacione.cpp
+++ inflacione.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 int main(){
 
-    double valor;
+    int valor;
+
+    cout<<"Digite o valor: ";
+    cin>>valor;
+
+    if(valor < 100){
+
+        int valor_inflacionado = valor*0.10;
+        int valor_total = valor+valor_inflacionado;
+
+        cout<<"O valor inflacionado eh: "<<valor_total;
+    }else if (valor >= 100)
+    {
+        int valor_inflacionado = valor*0.20;
+        int valor_total = valor+valor_inflacionado;
+
+        cout<<"O valor inflacionado eh: "<<valor_total;
+    }
+    
     
 }
